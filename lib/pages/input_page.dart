32f260809c00c5d480182c3ba8a15969b5516bc7
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-02-03 14:09:16 +0500

    imput page design implementation

---
+++ lib/pages/input_page.dart
@@ -0,0 +1,241 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/pages/result_page.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../bmi_calculator_brain.dart';
+import '../widgets/icon_content.dart';
+import '../widgets/reusable_card.dart';
+
+enum Gender {
+  male,
+  female,
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Gender? selectedGender;
+  int weight = 74;
+  int age = 15;
+  int height = 183;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          //first Expanded
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    child: ReusableCard(
+                      myColor: selectedGender == Gender.male
+                          ? activeCardColour
+                          : inActiveCardColor,
+                      cardChild: IconContent(
+                        text: 'MALE',
+                        myIcon: FontAwesomeIcons.mars,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    child: ReusableCard(
+                      myColor: selectedGender == Gender.female
+                          ? activeCardColour
+                          : inActiveCardColor,
+                      cardChild: IconContent(
+                        text: 'FEMALE',
+                        myIcon: FontAwesomeIcons.venus,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          //Second Expanded
+          Expanded(
+            child: ReusableCard(
+              myColor: inActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'HEIGHT',
+                    style: labelTextStyle,
+                  ),
+                  //height details
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic, ////<-- HERE --> ////
+                    children: [
+                      Text(
+                        height.toString(),
+                        style: numberTextStyle,
+                      ),
+                      const Text(
+                        'cm',
+                        style: labelTextStyle,
+                      ),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: const SliderThemeData(
+                      activeTrackColor: Colors.white,
+                      inactiveTrackColor: Color(0xFF4C4F5E),
+                      thumbColor: Color(0xFFEB1455),
+                    ),
+                    child: Slider(
+                      min: 120,
+                      max: 220,
+                      value: height.toDouble(),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          //Third Expanded
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    myColor: inActiveCardColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'WEIGHT',
+                          style: labelTextStyle,
+                        ),
+                        Text(
+                          weight.toString(),
+                          style: numberTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              },
+                              backgroundColor: const Color(0xFF4C4F5E),
+                              child: const FaIcon(FontAwesomeIcons.plus),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  weight--;
+                                });
+                              },
+                              backgroundColor: const Color(0xFF4C4F5E),
+                              child: const FaIcon(FontAwesomeIcons.minus),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    myColor: inActiveCardColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'AGE',
+                          style: labelTextStyle,
+                        ),
+                        Text(
+                          age.toString(),
+                          style: numberTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FloatingActionButton(
+                              child: const FaIcon(FontAwesomeIcons.minus),
+                              onPressed: () {
+                                setState(() {
+                                  age--;
+                                });
+                              },
+                              backgroundColor: const Color(0xFF4C4F5E),
+                            ),
+                            const SizedBox(width: 10),
+                            FloatingActionButton(
+                              onPressed: () {
+                                setState(() {
+                                  age++;
+                                });
+                              },
+                              child: const FaIcon(FontAwesomeIcons.plus),
+                              backgroundColor: const Color(0xFF4C4F5E),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+            },
+            child: Container(
+              height: 80,
+              width: double.infinity,
+              color: const Color(0xFFEB1455),
+              margin: const EdgeInsets.only(top: 10),
+              child: const Center(
+                child: Text(
+                  'CALCULATE',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// height = double.parse(newvalue.toStringAsFixed(1));
