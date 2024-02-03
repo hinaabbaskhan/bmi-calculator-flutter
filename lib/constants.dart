0bf0d1b3d441f16f6ed45c2e470c8436b57fc850
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-02-03 14:07:52 +0500

    const file added

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
 
-Color inActiveCardColor = Color(0xff1D1F33);
+const inActiveCardColor = Color(0xff1D1F33);
+const activeCardColour = Color(0xFF111328);
+const labelTextStyle = TextStyle(
+  fontSize: 18,
+  color: Color(0xffFFFFFF),
+);
+
+const numberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+);
